5fd7f99768d12534e5a104186bde7dc03bccdfd7
Harsh Guleria <guleriah59@gmail.com>
2025-12-22 16:57:56 +0530

    variables in python

---
+++ 03_variables/01_basics.py
@@ -0,0 +1,10 @@
+age = 34 # integer
+name = "Harry" # string
+cgpa = 4.55 # float
+
+# 34age = 4 # Invalid bariable because variable cannot start with a number
+
+age = 32 # Valid because variable can start with an alphabet
+
+# a$$ge = 45 # Invalid because variables cannot contain specila characters other than _
+_age = 34
---
+++ 03_variables/02_data_types.py
@@ -0,0 +1,26 @@
+# Python support s several buil-in data types:
+
+# Integers, 
+# Floats, 
+# Strings, 
+# Booleans, 
+# Lists, 
+# Tuples, 
+# Sets, 
+# Dictionaries
+
+age = 5
+print(age)
+print(type(age))
+
+cgpa = 9.2
+print(cgpa)
+print(type(cgpa))
+
+name= "Harsh"
+print(name)
+print(type(name))
+
+is_completed = True # can also be False
+print(is_completed)
+print(type(is_completed))
