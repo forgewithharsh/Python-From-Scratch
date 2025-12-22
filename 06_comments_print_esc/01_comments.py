b9ae2813386c26a6492e43641b7a8bc65dd1eb6d
Harsh Guleria <guleriah59@gmail.com>
2025-12-22 18:01:36 +0530

    comments,escape sequences & print statement in python

---
+++ 06_comments_print_esc/01_comments.py
@@ -0,0 +1,21 @@
+print("hey how are you?")
+
+# hey, please finish this code
+
+'''
+apple~$ python3 ./01_input.py
+Enter a number: 12
+12
+apple~$ python3 ./01_input.py
+Enter a number: 32
+32
+Enter your name: harsh
+harsh
+apple~$ python3 ./02_add.py  
+Enter first number23
+Enter first number1
+231
+apple~$ python3 ./02_add.py
+Enter first number: 3
+Enter first number: 3
+'''
---
+++ 06_comments_print_esc/03_print.py
@@ -0,0 +1,3 @@
+# print('Hello World',"harry",5,sep="/")
+print('Hello World', end="..")
+print('Harry', end="//")
