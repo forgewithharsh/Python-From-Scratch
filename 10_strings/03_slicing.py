c0d981c8c1f2dc101aad8ec85b4e64dec6843a2c
Harsh Guleria <guleriah59@gmail.com>
2025-12-23 16:56:53 +0530

    string slicing and indexing

---
+++ 10_strings/03_slicing.py
@@ -0,0 +1,13 @@
+name = "Harry"
+
+print(name[0:2]) # goes from 0 to 2-1 ie 0 to 1
+
+print(name[2:-1]) # same as name[2:4]
+
+# print(name[0:10:n]) # Skip n - 1 characters
+print(name[0:10:1]) # Skip 0 characters
+print(name[0:10:2]) # Skip 1 characters
+
+print(name[:4]) # Replace the first empty number with 0 # name[0: 4]
+
+print(name[1:]) # Replace the secon empty number with length
