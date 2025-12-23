188625078037250c3609a2a0443340720e2efab6
Harsh Guleria <guleriah59@gmail.com>
2025-12-23 09:31:50 +0530

    operators in python

---
+++ 07_operator.py/01_operator.py
@@ -0,0 +1,55 @@
+# Arthmetic Operator
+a = 34
+b = 2
+
+'''
+print("a + b = ", a + b)
+print("a - b = ", a - b)
+print("a * b = ", a * b)
+print("a / b = ", a / b)
+print("a % b = ", a % b)
+print("a // b = ", a // b)
+print("a ** b = ", a ** b)
+'''
+
+# Conditional Operator
+'''
+print(a>4)
+print(a<4)
+print(a<=4)
+print(a>=4)
+print(a==4) # Is a equal to 4?
+print(a==34) # Is a equal to 34?
+print(a!=34) # Is a not equal to 34?
+'''
+
+# Logical Operators
+'''
+c = True
+d = False
+
+print(c and c)
+print(c and d)
+print(d and c)
+print(d and d)
+
+print("For Or Operator...")
+print(c or c)
+print(c or d)
+print(d or c)
+print(d or d)
+
+print("Not Operator")
+print(not(True))
+print(not(False))
+'''
+
+# Assignment Operator
+a = 32
+print(a)
+
+# a += 3
+# print(a)
+
+a -= 3
+print(a)
