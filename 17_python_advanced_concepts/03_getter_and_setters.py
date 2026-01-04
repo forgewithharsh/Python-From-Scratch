cccf8a96bca8303efa3d236ace827efc0943ddbf
Harsh Guleria <guleriah59@gmail.com>
2026-01-04 16:10:04 +0530

    a git tutorial beginning

---
+++ 17_python_advanced_concepts/03_getter_and_setters.py
@@ -0,0 +1,10 @@
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+    def first_name(self):
+        return self.name.split
+
+e = Employee("Jack Doe", 34555)
+# e.projects = 6
+# print(e.projects)
