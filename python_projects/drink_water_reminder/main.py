e71a1cfc5219a3215c6fa5fe3ba9aa16f7a85e4b
Harsh Guleria <guleriah59@gmail.com>
2026-01-06 06:02:55 +0530

    build a drink water reminder app

--- python_projects/drink_water_reminder/main.py
+++ python_projects/drink_water_reminder/main.py
@@ -3,6 +3,6 @@ import os
 
 while True:
     os.system("""
-osascript -e 'display notification "You need to drink some water" with title "Hydration Reminder"'
-""")
+    osascript -e 'display notification "You need to drink some water" with title "Hydration Reminder"'
+    """)
     time.sleep(1800)
