8efe8abfcd9c3df5dc027883b465540e686b3dd5
Harsh Guleria <guleriah59@gmail.com>
2026-01-10 12:49:05 +0530

    build a news app using api

--- python_projects/news_api/main.py
+++ python_projects/news_api/main.py
@@ -3,14 +3,14 @@ import requests
 query = "artifical intelligence"
 api="2f9b9d5a99f74f71af8c7fb4beebd67c"
 
-url = "https://newsapi.org/v2/everything?q={query}&from=2025-12-10&sortBy=publishedAt&apiKey={api}"
+url = f"https://newsapi.org/v2/everything?q={query}&from=2025-12-10&sortBy=publishedAt&apiKey={api}"
 
 print(url)
 r = requests.get(url)
 
 data = r.json()
-
 articles = data["articles"]
-for article in articles:
-    print(article["title"], article["url"])
+
+for index, article in enumerate(articles):
+    print(index + 1, article["title"], article["url"])
     print("\n*********************************\n")
