b2a1dfadfaf747f6ebe7cd4c8dc5a85651d9cedb
Harsh Guleria <guleriah59@gmail.com>
2026-01-12 16:11:04 +0530

    import text to audio in generate_process

--- VidSnapAi/generate_process.py
+++ VidSnapAi/generate_process.py
@@ -1,7 +1,12 @@
 # This file looks for new folders inisde user uploads and converts them to reel if they are not already coverted
 import os
+from text_to_audio import text_to_speech_file
+import time
 def text_to_audio(folder):
     print("TTA - ",folder)
+    with open(f"user_uploads/{folder}/desc.txt") as f:
+        text = f.read()
+    # text_to_speech_file(text,folder)
 
 
 
@@ -9,14 +14,17 @@ def create_reel(folder):
     print("CR - ",folder)
 
 if __name__ == "__main__":
-    with open("done.txt", "r") as f:
-        done_folders = f.readlines()
+    while True:
+        print("Processing queue...")
+        with open("done.txt", "r") as f:
+            done_folders = f.readlines()
 
-    done_folders = [f.strip() for f in done_folders]
-    folders = os.listdir("user_uploads")
-    for folder in folders:
-        if(not folder in done_folders):
-            text_to_audio(folder) # Generate the audio.mp3 from desc.txt
-            create_reel(folder) # Convert the images and audio.mp3 inside the folder to a reel
-            with open("done.txt", "a") as f:
-                f.write(folder + "\n")
+        done_folders = [f.strip() for f in done_folders]
+        folders = os.listdir("user_uploads")
+        for folder in folders:
+            if(not folder in done_folders):
+                text_to_audio(folder) # Generate the audio.mp3 from desc.txt
+                create_reel(folder) # Convert the images and audio.mp3 inside the folder to a reel
+                with open("done.txt", "a") as f:
+                    f.write(folder + "\n")
+        time.sleep(4)
