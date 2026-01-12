fa16f1b8e987e2295daf4c8a55eb39845b197cdc
Harsh Guleria <guleriah59@gmail.com>
2026-01-13 00:26:28 +0530

    Generating Reels with audio using ffmpeg & Python

--- VidSnapAi/generate_process.py
+++ VidSnapAi/generate_process.py
@@ -7,12 +7,12 @@ def text_to_audio(folder):
     print("TTA - ",folder)
     with open(f"user_uploads/{folder}/desc.txt") as f:
         text = f.read()
-    # text_to_speech_file(text,folder)
+    text_to_speech_file(text,folder)
 
 
 
 def create_reel(folder):
-    command = ""
+    command = f'''./ffmpeg/ffmpeg -f concat -safe 0 -i user_uploads/{folder}/input.txt -i user_uploads/{folder}/audio.mp3 -vf "scale=1080:1920:force_original_aspect_ratio=decrease,pad=1080:1920:(ow-iw)/2:(oh-ih)/2:black" -c:v libx264 -c:a aac -shortest -r 30 -pix_fmt yuv420p static/reels/{folder}.mp4'''
     subprocess.run(command, shell=True, check=True)
 
     print("CR - ",folder)
