0c6bd3791db83cfc7f805dbf453ca63cfb7ce268
Harsh Guleria <guleriah59@gmail.com>
2026-01-12 17:16:30 +0530

    exception handling in python

--- 17_python_advanced_concepts/06_errors.py
+++ 17_python_advanced_concepts/06_errors.py
@@ -2,7 +2,13 @@ while True:
     try:
         a = int(input("Enter number 1: "))
         b = int(input("Enter number 2: "))
-        print(f"The sum is {a + b}")
+        print(f"The divison is {a / b}")
+
+    except ValueError:
+        print("Please dont perform bad typecasts")
+
+    except ZeroDivisionError:
+        print("Hey dont divide by 0")
 
     except Exception as e:
-        print("Some error occured!", e)
+        print("Unknown error occured!", e)
