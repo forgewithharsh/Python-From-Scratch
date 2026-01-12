db0b6cb3050170730176d72e8e5ce3140dc3b453
Harsh Guleria <guleriah59@gmail.com>
2026-01-12 17:19:22 +0530

    custom error throw

--- 17_python_advanced_concepts/06_errors.py
+++ 17_python_advanced_concepts/06_errors.py
@@ -1,14 +1,22 @@
-while True:
-    try:
-        a = int(input("Enter number 1: "))
-        b = int(input("Enter number 2: "))
-        print(f"The divison is {a / b}")
+# while True:
+#     try:
+#         a = int(input("Enter number 1: "))
+#         b = int(input("Enter number 2: "))
+#         print(f"The divison is {a / b}")
 
-    except ValueError:
-        print("Please dont perform bad typecasts")
+#     except ValueError:
+#         print("Please dont perform bad typecasts")
 
-    except ZeroDivisionError:
-        print("Hey dont divide by 0")
+#     except ZeroDivisionError:
+#         print("Hey dont divide by 0")
 
-    except Exception as e:
-        print("Unknown error occured!", e)
+#     except Exception as e:
+#         print("Unknown error occured!", e)
+
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+
+if b == 0:
+    raise ValueError("Please dont divide by 0")
+
+print(f"The divison is {a / b}")
